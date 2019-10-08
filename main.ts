0ad33a537df6717d7bdd315bfc94ad3ed207374c
lencz <czienskowski@live.de>
2019-10-08 12:22:04 +0200

    added min and max values for var

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 //% color=78 weight=100 icon="\uf493" block="Microbit Test Blocks"
 //% groups=['Microbit-Test']
 namespace microbitTest {
-    //% block
+    //% block v.min = 0 v.max = 42 x.defl = 25
     export function thisIsATest(v: number, interval: number = 150): void
     { }
 }
