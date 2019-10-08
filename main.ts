ad0f99ef849e7cdf5addb7759a4dc265652caa6a
lencz <czienskowski@live.de>
2019-10-08 11:48:44 +0200

    moved custom code to pxt-test

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * Provides access to basic micro:bit functionality.
+ */
+//% color=78 weight=100 icon="\uf493" block="Test Blocks"
+namespace test {
+    
+}
