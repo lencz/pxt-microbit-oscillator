0fab158b8651e4fac1b52f9457e7c4e071136a5a
lencz <czienskowski@live.de>
2019-10-08 15:25:08 +0200

    enum for shape

--- main.ts
+++ main.ts
@@ -1,13 +1,17 @@
 //% color=#1c1337 weight=0 icon="\uf1d1" block="Microbit Tryout"
 
 namespace tryout {
-    //% block="show|number %v" v.min = 0 v.max = 42 x.defl = 25
+    //% block="show|number %v"
     export function showNumber(v: number, interval: number = 150): void {
         console.log("" + v)
     }
 
-    //% block="Oscillator %v" v.min = 0 v.max = 42 x.defl = 25
-    export function secondFunction(v: number, interval: number = 150): void {
+    //% block="Oscillator %v"
+    //% enumName="Waveshape"
+    //% enumMemberName="waveshape"
+    //% enumPromptHint="Shape"
+    //% enumInitialMembers="Sawtooth, Triangle, Square, Sine"
+    export function secondFunction(v: number): void {
         console.log("" + v)
     }
 }
