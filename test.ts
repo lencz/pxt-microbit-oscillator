89631dc915848b683aaeb77915f6e5765eefff07
lencz <czienskowski@live.de>
2019-10-08 11:45:21 +0200

    created test file

--- test.ts
+++ test.ts
@@ -1,1 +1,9 @@
 // tests go here; this will not be compiled when this package is used as a library
+
+/**
+ * Provides access to basic micro:bit functionality.
+ */
+//% color=78 weight=100 icon="\uf493" block="Test Blocks"
+namespace test {
+
+}
