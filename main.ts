3383f9f8708b232cb57000aeb3160a5a1c0fe6bf
lencz <czienskowski@live.de>
2019-10-08 13:49:21 +0200

    trying to undestand how groups work

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
-//% color=78 weight=100 icon="\uf493" block="Math"
-//% groups=['math']
-namespace Math {
+//% color=78 weight=100 icon="\uf1ec" block="Microbit Tryout"
+//% groups=['Tryout']
+
+namespace Tryout {
     //% block="show|number %v" v.min = 0 v.max = 42 x.defl = 25
-    export function thisIsATest(v: number, interval: number = 150): void
-    {
-        console.log(""+v)
+    export function thisIsATest(v: number, interval: number = 150): void {
+        console.log("" + v)
     }
 }
