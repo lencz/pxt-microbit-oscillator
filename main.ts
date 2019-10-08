9fae0fcb0201d910ae223c35873a11cc2df572b0
lencz <czienskowski@live.de>
2019-10-08 11:54:49 +0200

    added export function

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-/**
- * Provides access to basic micro:bit functionality.
- */
-//% color=78 weight=100 icon="\uf493" block="Test Blocks"
+//% color=78 weight=100 icon="\uf493" block="Test Blocks" advanced=true
+//% groups=['Test']
 namespace test {
-    
+    //% block
+    export function showNumber(v: number, interval: number = 150): void
+    { }
 }
