d43d9e4a1802d7fa65e55a87d8b19119e1248d1d
lencz <czienskowski@live.de>
2019-10-08 14:28:02 +0200

    removed groups

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 //% color=#1c1337 weight=0 icon="\uf1d1" block="Microbit Tryout"
-//% groups=['Tryout']
 
 namespace tryout {
     //% block="show|number %v" v.min = 0 v.max = 42 x.defl = 25
@@ -7,7 +6,7 @@ namespace tryout {
         console.log("" + v)
     }
 
-    //% block="need|this|block|annotation %v" v.min = 0 v.max = 42 x.defl = 25
+    //% block="Oscillator %v" v.min = 0 v.max = 42 x.defl = 25
     export function secondFunction(v: number, interval: number = 150): void {
         console.log("" + v)
     }
