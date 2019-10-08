4d5780c3f338b08f0e22bf4ca48e0df55569459f
lencz <czienskowski@live.de>
2019-10-08 13:31:22 +0200

    try out blocks/namespaces

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
-//% color=78 weight=100 icon="\uf493" block="Microbit Test Blocks"
-//% groups=['Microbit-Test']
-namespace microbitTest {
+//% color=78 weight=100 icon="\uf493" block="Math"
+//% groups=['math']
+namespace Math {
     //% block="show|number %v" v.min = 0 v.max = 42 x.defl = 25
     export function thisIsATest(v: number, interval: number = 150): void
-    { }
+    {
+        console.log(""+v)
+    }
 }
