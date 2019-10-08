60a6da96ddcfb4c23c82743c89a24c8e3398224b
lencz <czienskowski@live.de>
2019-10-08 14:19:16 +0200

    renamed function

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 //% color=#1c1337 weight=0 icon="\uf1d1" block="Microbit Tryout"
 //% groups=['Tryout']
 
-namespace Tryout {
+namespace tryout {
     //% block="show|number %v" v.min = 0 v.max = 42 x.defl = 25
-    export function thisIsATest(v: number, interval: number = 150): void {
+    export function showNumber(v: number, interval: number = 150): void {
         console.log("" + v)
     }
 }
