eaba928b1a888aa2c528b833a06ae4059fc614d4
Dario Salvi <dariosalvi78@gmail.com>
2020-01-20 13:05:25 +0100

    made into a class

--- main.ts
+++ main.ts
@@ -12,93 +12,101 @@ enum WaveShape {
 //% color=#1ca7c1 weight=0 icon="\uf1d1" block="Oscillator"
 namespace oscillator {
 
-    let timeOfLastTrigger: number = 0
-    let timeSinceLastTrigger: number = 0
-    let state: number = 0
+    export class Oscillator {
+        timeOfLastTrigger: number
+        timeSinceLastTrigger: number
+        state: number
 
-    //% block="Create wave %w with period of (ms) %period"
-    //% enumName="WaveShape"
-    //% enumMemberName="waveshape"
-    //% enumPromptHint="Shape"
-    //% enumInitialMembers="Sawtooth, Triangle, Square, Sine"
-    //% period.defl=1000
-    export function oscillate(w: WaveShape, period: number): number {
-        switch (w) {
-            case 0: {
-                return (sawtoothWave(period))
-                break;
-            }
-            case 1: {
-                return (sinusWave(period))
-                break;
-            }
-            case 2: {
-                return (squareWave(period))
-                break;
-            }
-            case 3: {
-                return (triangleWave(period))
-                break;
-            }
-            default: {
-                return (0);
-                break;
+        constructor() {
+            this.timeOfLastTrigger = 0
+            this.timeSinceLastTrigger = 0
+            this.state = 0
+        }
+
+        //% block="oscillate %w wave with period of (ms) %period"
+        //% enumName="WaveShape"
+        //% enumMemberName="waveshape"
+        //% enumPromptHint="Shape"
+        //% enumInitialMembers="Sawtooth, Triangle, Square, Sine"
+        //% period.defl=1000
+        public oscillate(w: WaveShape, period: number): number {
+            switch (w) {
+                case 0: {
+                    return (this.sawtoothWave(period))
+                    break;
+                }
+                case 1: {
+                    return (this.sinusWave(period))
+                    break;
+                }
+                case 2: {
+                    return (this.squareWave(period))
+                    break;
+                }
+                case 3: {
+                    return (this.triangleWave(period))
+                    break;
+                }
+                default: {
+                    return (0);
+                    break;
+                }
             }
         }
-    }
 
-    function sawtoothWave(period: number): number {
-        let now: number
-        now = input.runningTime()
-        timeSinceLastTrigger = now - timeOfLastTrigger
-        if (timeSinceLastTrigger >= period) {
-            timeOfLastTrigger = now
-            timeSinceLastTrigger = 0
+        sawtoothWave(period: number): number {
+            let now: number
+            now = input.runningTime()
+            this.timeSinceLastTrigger = now - this.timeOfLastTrigger
+            if (this.timeSinceLastTrigger >= period) {
+                this.timeOfLastTrigger = now
+                this.timeSinceLastTrigger = 0
+            }
+            return (Math.constrain(this.timeSinceLastTrigger, 0, period))
         }
-        return (Math.constrain(timeSinceLastTrigger, 0, period))
-    }
 
-    function sinusWave(period: number): number {
-        let value: number
-        let now: number
-        now = input.runningTime()
-        value = now / (1000 / (2 * Math.PI))
-        return (Math.sin(value))
-    }
+        sinusWave(period: number): number {
+            let value: number
+            let now: number
+            now = input.runningTime()
+            value = now / (1000 / (2 * Math.PI))
+            return (Math.sin(value))
+        }
 
-    function squareWave(period: number): number {
-        let now: number
-        now = input.runningTime()
-        timeSinceLastTrigger = now - timeOfLastTrigger
-        if (timeSinceLastTrigger >= period / 2) {
-            if (state) {
-                state = 0;
+        squareWave(period: number): number {
+            let now: number
+            now = input.runningTime()
+            this.timeSinceLastTrigger = now - this.timeOfLastTrigger
+            if (this.timeSinceLastTrigger >= period / 2) {
+                if (this.state) {
+                    this.state = 0;
+                } else {
+                    this.state = 1;
+                }
+                this.timeOfLastTrigger = now
+                this.timeSinceLastTrigger = 0
+            }
+            return (this.state)
+        }
+
+        triangleWave(period: number): number {
+            let now: number
+            now = input.runningTime()
+            this.timeSinceLastTrigger = now - this.timeOfLastTrigger
+            if (this.timeSinceLastTrigger >= period / 2) {
+                if (this.state) {
+                    this.state = 0;
+                } else {
+                    this.state = 1;
+                }
+                this.timeOfLastTrigger = now
+                this.timeSinceLastTrigger = 0
+            }
+            if (this.state) {
+                return (Math.map(this.timeSinceLastTrigger, 0, period / 2, 0, 1000))
             } else {
-                state = 1;
+                return (Math.map(this.timeSinceLastTrigger, 0, period / 2, 1000, 0))
             }
-            timeOfLastTrigger = now
-            timeSinceLastTrigger = 0
-        }
-        return (state)
-    }
-
-    function triangleWave(period: number): number {
-        let now: number
-        now = input.runningTime()
-        timeSinceLastTrigger = now - timeOfLastTrigger
-        if (timeSinceLastTrigger >= period / 2) {
-            if (state) {
-                state = 0;
-            } else {
-                state = 1;
-            }
-            timeOfLastTrigger = now
-            timeSinceLastTrigger = 0
-        }
-        if (state) {
-            return (Math.map(timeSinceLastTrigger, 0, period / 2, 0, 1000))
-        } else {
-            return (Math.map(timeSinceLastTrigger, 0, period / 2, 1000, 0))
         }
     }
 }
